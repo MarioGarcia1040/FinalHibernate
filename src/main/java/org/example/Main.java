ae52a2a8f2f351ebcd9e1e2b97f58f6f9c5fc9ca
MarioGarcia1040 <mariogarcia1040@gmail.com>
2025-02-03 15:30:51 -0600

    Proyecto final de SQL, JDBC e Hibernate.
    Realizando pruebas de rendimiento MySQLy cache con Redis.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Proyecto final de SQL, JDBC e Hibernate.\n" +
+                "\n" +
+                "Realizando pruebas de rendimiento MySQLy cache con Redis.\n");
     }
 }
